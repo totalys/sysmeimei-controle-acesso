0cea8a9bca3f3ddbb1cb23dd6deb061d1668d4c9
Tiago Martins de Alexandre <t.alexandre@gmail.com>
2025-08-15 22:35:14 -0300

    feat: Permite cadastrar presença de assistidos também

--- consumidor/sysmeimei-ac-consumer.py
+++ consumidor/sysmeimei-ac-consumer.py
@@ -9,8 +9,8 @@ from models import Employee, Student, Assistido
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", 5672))
 QUEUE_NAME = os.getenv("QUEUE_NAME", "lar_meimei_access")
-
-POST_URL = os.getenv("POST_URL", "https://larmeimei.org/api/resource/LM%20Attendance")
+EMPLOYEE_URL = os.getenv("EMPLOYEE_URL", "https://larmeimei.org/api/resource/LM%20Attendance")
+CUSTOMER_URL = os.getenv("CUSTOMER_URL", "https://larmeimei.org/api/resource/LM%20Attendance%20cst")
 POST_HEADERS = json.loads(os.getenv(
     "POST_HEADERS",
     '{"Content-Type": "application/json", "accept": "application/json"}'
@@ -31,10 +31,13 @@ def parse_message(perfil, area, data):
     cls = PROFILE_MAP.get(key)
     if not cls:
         raise ValueError(f"Tipo de payload não mapeado: perfil={perfil}, area={area}")
-    return cls(**data)
+    
+    post_url = EMPLOYEE_URL if perfil == "voluntario" else CUSTOMER_URL
+    
+    return cls(**data), post_url
 
-def send_to_api(obj):
-    response = requests.post(POST_URL, headers=POST_HEADERS, json=obj.__dict__)
+def send_to_api(obj, post_url):
+    response = requests.post(post_url, headers=POST_HEADERS, json=obj.__dict__)
     print(f"[API] Status={response.status_code}, Body={response.text}")
 
 def callback(ch, method, properties, body):
@@ -43,7 +46,7 @@ def callback(ch, method, properties, body):
         area = properties.headers.get("area")
         payload = json.loads(body)
         
-        obj = parse_message(perfil, area, payload)
+        obj, post_url = parse_message(perfil, area, payload)
         print(f"[✓] Recebido: {obj}")
 
         send_to_api(obj)
